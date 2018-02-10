93ab1e1b6608b20b40e47471a0cab15ea6cdd0ad
Hyt <HytMiao@outlook.com>
2018-02-10 23:29:08 +0800

    ·修改了动态亮灯的方法，但是反应速度仍然不足

--- script/blocks/Scanner.lua
+++ script/blocks/Scanner.lua
@@ -1,5 +1,6 @@
 --公共变量存储位置：19200,5,19200
 IgnoreId = {0,212};
+Lights = {};
 function main(entity)
 	local globalData = blocks.getscript(19200,5,19200,true);
 	bx,by,bz = entity:GetBlockPos();
@@ -8,6 +9,8 @@ function main(entity)
 	cmd("/tip -notice -color #cccc00 音乐已暂停");
 	return
 	end
+	--关灯
+	ClearLights();
 	--获取开始位置
 	local beginX;
 	if(globalData.lineIndex > 1) then
@@ -16,11 +19,19 @@ function main(entity)
 		globalData.lineIndex = 1;
 		beginX = globalData.lineIndex + globalData.beginPositionX - 1;
 	end
-	local beginY = globalData.beginPositionY;
-	local beginZ = globalData.beginPositionZ;
 
 	--扫描
-	for Z = globalData.mapWidth, 1, -1 do
+	ScanLine(beginX,globalData.beginPositionY,globalData.beginPositionZ,globalData.mapWidth);
+	ApplyLights();
+	cmd("/tip -debug It is working at "..globalData.lineIndex..".");
+	globalData.lineIndex  = globalData.lineIndex + 1;
+	--创建自循环
+	entity:SetFrameMoveInterval(1/15);
+	cmd("/t 0.01/activate",nil,entity);
+end
+
+function ScanLine(beginX,beginY,beginZ,mapWidth)
+	for Z = mapWidth, 1, -1 do
 		--检查是否在忽略列表内
 		local Id = BlockEngine:GetBlockId(beginX,beginY,Z + beginZ - 1);
 		local ignore = false;
@@ -32,29 +43,39 @@ function main(entity)
 		--midi偏移量：67
 		--因为是从右往左扫描，所以应该是(129-z)+67=196-z
 		cmd("/midi "..(196 - Z));
-		Light(129 - Z);
+		PianoLight(129 - Z);
 		end
-		cmd(string.format("/setblock %i %i %i 6", beginX, beginY + 1, beginZ));
-		cmd(string.format("/t 0.01/setblock %i %i %i 0", beginX, beginY + 1, beginZ));
+		AddLight(beginX,beginY + 1,beginZ);
 	end
-	cmd("/tip -debug It is working at "..globalData.lineIndex..".");
-	globalData.lineIndex  = globalData.lineIndex + 1;
-	--创建自循环
-	entity:SetFrameMoveInterval(1/15);
-	cmd("/t 0.01/activate",nil,entity);
+	return 0;
 end
 
---亮灯
+--添加新的灯
+function AddLight(x,y,z)
+	table.insert(Lights,1,{x,y,z});
+end	
+--应用所有的灯
+function ApplyLights()
+	for k,position in ipairs(Lights) do
+		BlockEngine:SetBlock(position[1],position[2],position[3],6,1,0);
+	end
+end
+--删去所有的灯
+function ClearLights()
+	for k,position in ipairs(Lights) do
+		BlockEngine:SetBlock(position[1],position[2],position[3],0,1,0);
+	end
+	Lights = {};
+end
+
+--在琴键上亮灯
 --note：音高
-function Light(note)
+function PianoLight(note)
 	local x,y,z = NoteToLightPosition(note);
-	cmd("/setblock "..x.." "..y.." "..z.." 6");
-	cmd("/t 0.1/setblock "..x.." "..y.." "..z.." 0");
-	echo("x:"..x.."y:"..y.."z:"..z);
+	AddLight(x,y,z);
 end
-
 --[硬编码]根据音符获取相应的灯的位置
-function NoteToLightPosition(note)
+function NoteToPianoLightPosition(note)
 	local beginX = 19198;
 	local beginY = 6;
 	local beginZ = 19200;
