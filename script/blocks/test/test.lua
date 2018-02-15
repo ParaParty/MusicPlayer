da6ad9119afef390a17cc11f46b24841ff1209ec
Hyt <HytMiao@outlook.com>
2018-02-16 01:11:21 +0800

    `链接到了ParaLib，等待ParaLib的开发

--- script/blocks/test/test.lua
+++ script/blocks/test/test.lua
@@ -1,20 +1,6 @@
 function main(entity)
-	--[[x,y,z = entity:GetBlockPos();
-	echo("ID:"..BlockEngine:GetBlockId(x,y + 1,z));
-	tb = BlockEngine:GetBlockEntityData(x,y + 1,z);
-	BlockEngine:SetBlock(x,y+2,z,BlockEngine:GetBlockId(x,y+1,z));
-	BlockEngine:SetBlockData(x,y+2,z,tb);
-	--[[for k,value in ipairs(tb) do
-		echo("   "..k..": "..value);
-	end]]
-	--[[for k = 1, table.maxn(tb) do
-		echo("  "..k..": "..tb[k][1]);
-	end]]
-	--echo("inventory: "..tb.inventory);
-	
+	local ParaLib = blocks.getscript(19200,1,19200).ParaLib;
+	ParaLib:CreateSBlock(entity:GetBlockPos()):SimpleMoveRelative(0,1,0);
 end
-
-
-
 
 
