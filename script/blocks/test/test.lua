53a88c86e036dcd064c6d3bcffd524b43825a3a8
hytmiao <hytmiao@outlook.com>
2018-02-17 18:06:51 +0800

    `test commit

--- script/blocks/test/test.lua
+++ script/blocks/test/test.lua
@@ -1,6 +1,10 @@
 function main(entity)
 	local ParaLib = blocks.getscript(19200,1,19200).ParaLib;
-	ParaLib:CreateSBlock(entity:GetBlockPos()):SimpleMoveRelative(0,1,0);
+	--ParaLib:CreateSBlock(entity:GetBlockPos()):SimpleMoveRelative(0,1,0);
+	x, y, z = entity:GetBlockPos();
+	ParaLib:Translate(x, y + 1, z,
+					0, 1, 0,
+					0, 1, 0);
 end
 
 
